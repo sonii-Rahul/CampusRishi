b7a94a267396c868811313e866ae5e2a79605e1c
Rahul Soni <rahuls5788@gmail.com>
2024-03-28 21:18:38 +0530

    latest 28-04

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -24,7 +24,6 @@ function home() {
     <SectionFive/>
     <SectionFour/>
     <FaqThree/>
-   
     <Footer/>
     
     </motion.div>
--- src/Components/admin/admin.jsx
+++ src/Components/admin/admin.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
-
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Axios from 'axios';
+import Cookies from 'js-cookie';
+import UserContext from '../context/userContext.js';
+import SideBar from './DashBoard/SideBar.jsx';
 
 
 function admin() {
-  const location = useLocation();
-const { user } = location.state;
+
+const location = useLocation();
+const navigate = useNavigate();
+const {user,setUser} = useContext(UserContext)
+
+useEffect(() => {
+  const checkToken = async () => {
+    const accessToken = Cookies.get('accessToken');
+    if (!accessToken) {
+      navigate('/login');
+    } else {
+      try {
+        const response = await Axios.post('/api/v1/users/verify', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (response.status === 200) {
+          console.log(response.data.data)
+          setUser(response.data.data.user);
+        }
+      } catch (error) {
+        console.error('An error occurred during token verification:', error);
+        
+      }
+    }
+  };
+  checkToken();
+}, [navigate,setUser]);
+
+const handleLogout = async () => {
+  try {
+    const response = await Axios.post(
+      '/api/v1/users/logout',
+      {},
+      { withCredentials: true }
+    );
+    if (response.status === 200) {
+      navigate('/login');
+    }
+  } catch (error) {
+    console.error('An error occurred during logout:', error);
+  }
+};
+
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-    <div>welcome {user.username} admin</div>
+    <SideBar logingout={handleLogout} />
   
     </>
   )
